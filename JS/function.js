81b93429153e60947ef39e46e2b570651cd514f4
BOUNCE8 <43513640+BOUNCE8@users.noreply.github.com>
2020-07-03 18:03:10 +0100

    Add files via upload

--- JS/function.js
+++ JS/function.js
@@ -4,61 +4,17 @@ new fullpage('#fullpage', {
     anchors:['firstPage', 'secondPage', 'thirdPage', 'forthPage'],
     onLeave: (origin, destination, direction) => {
         const section = destination.item;
-        const heroTitle = section.querySelector('#heroTitle');
-        const heroText = section.querySelector('.hero-title-text');
+        // const heroTitle = section.querySelector('#heroTitle');
+        // const heroText = section.querySelector('.hero-title-text');
         
-        // const projectImages = document.querySelectorAll('.project-image');
-        // const socialIcons = document.querySelectorAll('.social-icon');
+        // const tl = new TimelineMax({delay: 0.2});
+        // const t2 = new TimelineMax({delay: 0.6});
         
-        const tl = new TimelineMax({delay: 0.2});
-        const t2 = new TimelineMax({delay: 0.6});
-        // const t3 = new TimelineMax({delay: 0.6});
-        // const t4 = new TimelineMax({delay: 1});
-        // const t5 = new TimelineMax({delay: 1});
-        // const t6 = new TimelineMax({delay: 1.4});
-        // const t7 = new TimelineMax({delay: 1.4});
-        
-        tl.fromTo(heroTitle,0.5, {y:'-50', opacity: 0}, {y:0, opacity: 1})
-        .fromTo(heroText, 0.5, {opacity: 0}, {opacity: 1})
+        // tl.fromTo(heroTitle,0.5, {y:'-50', opacity: 0}, {y:0, opacity: 1})
+        // .fromTo(heroText, 0.5, {opacity: 0}, {opacity: 1})
 
-        // Recent Work Page
-
-
-        // Replace Current animation with hover on / off when each project is highlighted
-        // if(destination.index === 1) {
-        //     const projectInfo = document.querySelectorAll('.project-text');
-        //     t2.fromTo(projectInfo, 0.6, {y:"0"}, {y: "175"});
-        // }
-        // Replace Current animation with hover on / off when each project is highlighted
     }
 });
-
-
-
-
-// document.querySelectorAll('.project-image').addEventListener("mouseover", function() {
-//     console.log("Guiltyyyyyyy")
-// });
-// const projectImages = document.querySelectorAll('.project-image');
-
-// projectImages.forEach(proImage => proImage.addEventListener('click', function() {
-    
-//     TweenMax.to(proImage, 0.5, {x: '-300px'})
-    // Close but need to figure out how to seperate the animations when clicked
-    // if (this.style.transform = "translate(0px, 0px)") {
-    //     gsap.from(proImage, 0.5, {x: '-300px'}) 
-
-    //     } else if (this.style.transform = "translate(-300px, 0px)") {
-    //         gsap.to(proImage, 0.5, {x: '300px'}) 
-    //     } else {
-    //         console.log("ahoyyyy")
-    //     }
-//     }
-// ));
-// projectImages.forEach(proImage => proImage.addEventListener('click', function() {
-//     TweenMax.to(proImage, 0.5, {x: '300px'})
-    
-// }));
 
 // Hamburger - Drop down menu function
 const hamburger = document.querySelector('#hamburger');
@@ -71,9 +27,7 @@ const dropDownMenu = document.querySelector('.sticky-nav-container')
 
                 dropDownMenu.addEventListener('click', () => {
                     dropDownMenu.classList.toggle('openBurgerMenu');
-                });
-         
-    
+                });   
 // Hamburger - Drop down menu function
 
 // Contact Form - contact form function - START
@@ -84,7 +38,6 @@ const closeBtn = document.querySelector('.closeBtn');
 
 function showContactForm() {
     contactBtn.addEventListener('click', function(){
-        
         contactForm.style.visibility = "visible";
     })
 }
@@ -92,7 +45,6 @@ function showContactForm() {
 function closeContactForm() {
     closeBtn.addEventListener('click', function(){
         contactForm.style.visibility = "hidden";
-        
     })
 }
 
@@ -101,16 +53,15 @@ closeContactForm()
 
 // Contact Form - contact form function - END
 
+// my work page: modal functions
 const projectImage = document.querySelector('.project-image')
 const modal = document.querySelector('.modal');
 const modalCloseBtn = document.querySelector('.modalCloseBtn');
-
 const allProjects = document.querySelectorAll('.project-container .project .project-image');
 let option1 = document.querySelector('#projectImage-1-modal');
 let option2 = document.querySelector('#projectImage-2-modal');
 let option3 = document.querySelector('#projectImage-3-modal');
 let selectedProject = "-modal";
-
 
 
 for(project of allProjects){
